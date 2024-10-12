f7645ed6b65fec26944ab90d3d9f5f58d56918a3
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-10-12 20:43:45 +0900

    전역변수 지워짐 주의요함

--- WindowsProject1/Main.cpp
+++ WindowsProject1/Main.cpp
@@ -7,7 +7,7 @@
 #include "..\LeeSource\LeeApplication.h"
 
 
-Application app;
+
 
 #define MAX_LOADSTRING 100
 
@@ -15,6 +15,12 @@ Application app;
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다: 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -25,7 +31,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    app.test();
 
     // TODO: 여기에 코드를 입력합니다.
 
